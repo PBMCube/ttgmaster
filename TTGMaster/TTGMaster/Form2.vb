add8928dc8e3d580e9b6df83555bcb7edbb18ca3
SouthpawHare <SouthpawHare@82de0b32-166f-11df-acdb-972e972a076a>
2010-03-03 18:17:29 +0000

    Continued work on code

--- TTGMaster/TTGMaster/Form2.vb
+++ TTGMaster/TTGMaster/Form2.vb
@@ -1,3 +1,14 @@
-﻿Public Class ConnectionForm
+﻿Public Class Form2
 
+    Private Sub RadioButton1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton1.CheckedChanged
+        HostGroup.Enabled = True
+        JoinGroup.Enabled = False
+        ConnectButton.Enabled = True
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton2.CheckedChanged
+        HostGroup.Enabled = False
+        JoinGroup.Enabled = True
+        ConnectButton.Enabled = True
+    End Sub
 End Class
