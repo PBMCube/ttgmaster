13a29008245dba91462bd82c4d17f27eccb262d6
SouthpawHare <SouthpawHare@82de0b32-166f-11df-acdb-972e972a076a>
2010-03-03 17:43:59 +0000

    Updated code

--- TTGMaster/TTGMaster/Form1.Designer.vb
+++ TTGMaster/TTGMaster/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class MainForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,128 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ChatInputBox = New System.Windows.Forms.TextBox
+        Me.ChatSendButton = New System.Windows.Forms.Button
+        Me.PlayerList = New System.Windows.Forms.TextBox
+        Me.ChatBox = New System.Windows.Forms.TextBox
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ConnectToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ChatInputBox
+        '
+        Me.ChatInputBox.Location = New System.Drawing.Point(194, 481)
+        Me.ChatInputBox.Name = "ChatInputBox"
+        Me.ChatInputBox.Size = New System.Drawing.Size(297, 20)
+        Me.ChatInputBox.TabIndex = 0
+        '
+        'ChatSendButton
+        '
+        Me.ChatSendButton.Location = New System.Drawing.Point(497, 479)
+        Me.ChatSendButton.Name = "ChatSendButton"
+        Me.ChatSendButton.Size = New System.Drawing.Size(75, 23)
+        Me.ChatSendButton.TabIndex = 1
+        Me.ChatSendButton.Text = "Send"
+        Me.ChatSendButton.UseVisualStyleBackColor = True
+        '
+        'PlayerList
+        '
+        Me.PlayerList.Location = New System.Drawing.Point(12, 313)
+        Me.PlayerList.Multiline = True
+        Me.PlayerList.Name = "PlayerList"
+        Me.PlayerList.ReadOnly = True
+        Me.PlayerList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.PlayerList.Size = New System.Drawing.Size(176, 188)
+        Me.PlayerList.TabIndex = 2
+        '
+        'ChatBox
+        '
+        Me.ChatBox.Location = New System.Drawing.Point(194, 313)
+        Me.ChatBox.Multiline = True
+        Me.ChatBox.Name = "ChatBox"
+        Me.ChatBox.ReadOnly = True
+        Me.ChatBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.ChatBox.Size = New System.Drawing.Size(378, 160)
+        Me.ChatBox.TabIndex = 3
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(584, 24)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ConnectToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.F), System.Windows.Forms.Keys)
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.H), System.Windows.Forms.Keys)
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'ConnectToolStripMenuItem
+        '
+        Me.ConnectToolStripMenuItem.Name = "ConnectToolStripMenuItem"
+        Me.ConnectToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ConnectToolStripMenuItem.Text = "Connect"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(584, 514)
+        Me.Controls.Add(Me.ChatBox)
+        Me.Controls.Add(Me.PlayerList)
+        Me.Controls.Add(Me.ChatSendButton)
+        Me.Controls.Add(Me.ChatInputBox)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.Name = "MainForm"
+        Me.Text = "TTGMaster"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents ChatInputBox As System.Windows.Forms.TextBox
+    Friend WithEvents ChatSendButton As System.Windows.Forms.Button
+    Friend WithEvents PlayerList As System.Windows.Forms.TextBox
+    Friend WithEvents ChatBox As System.Windows.Forms.TextBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ConnectToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
 
 End Class
--- TTGMaster/TTGMaster/Form1.vb
+++ TTGMaster/TTGMaster/Form1.vb
@@ -1,3 +1,18 @@
-﻿Public Class Form1
+﻿Public Class MainForm
 
+    Dim versionNum As String = "v0.1.0"
+
+    Private Sub AboutToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutToolStripMenuItem.Click
+        Dim temp As String = "Tabletop Game Master " + versionNum + vbNewLine
+        temp += "Created by Team Awesome"
+        MsgBox(temp, MsgBoxStyle.OkOnly, "About")
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ConnectToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ConnectToolStripMenuItem.Click
+
+    End Sub
 End Class
